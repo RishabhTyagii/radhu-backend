526bb9e2d23d267a0c9072c57f34687741cf0055
RishabhTyagii <rishabtyagi55@gmail.com>
2026-08-16 20:24:18 +0530

    Import authentication_classes in tallysync views

--- tallysync/views.py
+++ tallysync/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
